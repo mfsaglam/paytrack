882cd341be587e0186e89495f9efb5dd2530941f
mfsaglam <mfsaglam1@gmail.com>
2023-12-11 05:33:43 +0300

    Initial Commit

---
+++ InstallmentCalculator/InstallmentCalculator.h
@@ -0,0 +1,18 @@
+//
+//  InstallmentCalculator.h
+//  InstallmentCalculator
+//
+//  Created by Fatih Sağlam on 11.12.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstallmentCalculator.
+FOUNDATION_EXPORT double InstallmentCalculatorVersionNumber;
+
+//! Project version string for InstallmentCalculator.
+FOUNDATION_EXPORT const unsigned char InstallmentCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstallmentCalculator/PublicHeader.h>
+
+
