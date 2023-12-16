d73c02e116aeae93aeefddce1d9a9537aa2c1933
mfsaglam <mfsaglam1@gmail.com>
2023-12-16 10:04:35 +0300

    remove unnecessary case from InstallmentTestDate

--- InstallmentCalculatorTests/Helpers/Helpers.swift
+++ InstallmentCalculatorTests/Helpers/Helpers.swift
@@ -10,7 +10,6 @@ import Foundation
 enum InstallmentTestDate: TimeInterval {
     case tomorrow = 86400 // 24 hours
     case threeMonthsAgo = -7862401 // -91 days
-    case threeDaysFromNow = 259200 // 3 days
     
     var date: Date {
         return Date().addingTimeInterval(rawValue)
