f0c59e11fb3e61cf066fb7e406115385628c2af1
mfsaglam <mfsaglam1@gmail.com>
2023-12-16 09:15:23 +0300

    extract Installment into its own file

--- InstallmentCalculator/Calculator.swift
+++ InstallmentCalculator/Calculator.swift
@@ -7,27 +7,6 @@
 
 import Foundation
 
-struct Installment {
-    var monthlyPayment: Double
-    var months: Int
-    var startingDate: Date
-    
-    var remainingMonths: Int {
-        let passedMonths = Date().months(from: startingDate)
-        return months - passedMonths
-    }
-    
-    var remainingAmount: Double {
-        Double(remainingMonths) * monthlyPayment
-    }
-
-    init(monthlyPayment: Double, months: Int, startingDate: Date) {
-        self.monthlyPayment = monthlyPayment
-        self.months = months
-        self.startingDate = startingDate
-    }
-}
-
 protocol CalculatorDelegate {
     func result(_ result: CalculationResult)
 }
