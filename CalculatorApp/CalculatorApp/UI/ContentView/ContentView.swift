560b8c79aa160e7b5b44c214f05a8192bf9b128f
mfsaglam <mfsaglam1@gmail.com>
2024-02-15 01:45:58 +0300

    move content view view model into its own file

--- CalculatorApp/CalculatorApp/UI/ContentView/ContentView.swift
+++ CalculatorApp/CalculatorApp/UI/ContentView/ContentView.swift
@@ -7,41 +7,6 @@
 
 import SwiftUI
 
-final class ContentViewViewModel: ObservableObject {
-    let presenter: CalculationResultsPresenter?
-    
-    @Published var result: PresentableResult?
-    @Published var installments: [PresentableInstallment]
-    var isEmpty: Bool {
-        installments.isEmpty
-    }
-    
-    
-    init(presenter: CalculationResultsPresenter) {
-        self.presenter = presenter
-        
-        self.result = presenter.presentableResult
-        self.installments = presenter.presentableInstallments
-    }
-    
-    private init(result: PresentableResult, installments: [PresentableInstallment]) {
-        self.presenter = nil
-        self.result = result
-        self.installments = installments
-    }
-}
-
-extension ContentViewViewModel {
-    static func forPreview() -> ContentViewViewModel {
-        .init(
-            result: .init(totalAmount: "123", currentlyPaying: "12", remainingMonths: "12"),
-            installments: [
-                .init(id: .init(), name: "iPhone", paymentDay: 2, paidMonths: 2, totalMonths: 12, remainingAmount: 500, monthlyPayment: 59.90)
-            ]
-        )
-    }
-}
-
 struct ContentView: View {
     @StateObject var viewModel: ContentViewViewModel
     
